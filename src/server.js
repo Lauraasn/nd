e74f1cbf2c5fc17122f6c91d515ec5482e56b10e
Laura Nunes <laura.a.sousa.nunes@gmail.com>
2021-10-22 10:00:57 -0400

    Sequelize foi instalado e node_modules foi adicionado ao gitignore

--- src/server.js
+++ src/server.js
@@ -2,18 +2,31 @@ const express = require("express");
 
 const app = express();
 
+// configuração do parser para requisições post
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }))
 
+/*
 const pool = require('./dao/conexao');
+*/
 
+// colocar servidor no ar
 const PORTA = process.env.PORT || 8080;
 app.listen(PORTA,function() {
-    console.log('Servidor rodando na porta 8080');
+    console.log(`Servidor rodando na porta ${PORTA}`); //antes era "8080" no lugar de "${PORTA}"
 });
 
+// rotas
+app.get('/teste', (req, res) => res
+    .status(200)
+    .send({
+        mensagem: 'Bem vinda!'
+    })
+);
+
+/*
 app.use('/publico', express.static(__dirname + '/publico'));
 
 app.get('/album', function(req, resp) {
@@ -44,3 +57,4 @@ app.post('/album-digital', function(req, resp) {
 
     resp.sendFile(__dirname + '/views/pagAlbum.html');
 });
+*/
